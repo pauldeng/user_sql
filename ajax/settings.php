a224fdc607bbfb4f77863abb36d35721a32bbf3a
Paul Deng <paul.deng@freestyletechnology.com.au>
2018-01-12 11:50:30 +1100

    Add Check if groupTable is set ot not

--- ajax/settings.php
+++ ajax/settings.php
@@ -233,7 +233,7 @@ if(isset($_POST['appname']) && ($_POST['appname'] === 'user_sql') && isset($_POS
                 'tablePrefix' => ''
             );
 
-            if ($_POST['groupTable'] === 'true') {
+            if ((isset($_POST['groupTable'])) && ($_POST['groupTable'] === 'true')) {
                 $sql_table = $_POST['sql_group_table'];
             } else {
                 $sql_table = $_POST['sql_table'];
